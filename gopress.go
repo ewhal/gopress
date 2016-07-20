e14fa557797a0c290943f6f198c9d18d5cb97fe5
Eliot Whalan <ewhal@pantsu.cat>
2016-07-20 10:28:52 +1000

    Add base function stuff

--- gopress.go
+++ gopress.go
@@ -1,21 +1,100 @@
 package main
 
 import (
+	"database/sql"
+	"html"
 	"log"
 	"net/http"
 
+	_ "github.com/go-sql-driver/mysql"
 	"github.com/gorilla/mux"
+	"github.com/microcosm-cc/bluemonday"
+	"github.com/russross/blackfriday"
 )
 
+type Page stuct {
+	Title string
+	Post string
+	Date string
+	Author string
+	Email string
+
+}
+
+// checkErr logger
+func checkErr(err error) {
+	if err != nil {
+		log.Println(err)
+	}
+}
+
 func rootHandler(w http.ResponseWriter, r *http.Request) {
+	db, err := sql.Open("mysql", DATABASE)
+	checkErr(err)
+	defer db.Close()
+
+	query, err := db.Query("select * from post")
+	checkErr(err)
+
+	b := []Page{}
+	for query.Next() {
+		p := Page{}
+		var input string
+		query.Scan(&p.Title, &input, &p.Date, &p.Author, &p.Email)
+		unsafe := blackfriday.MarkdownCommon(input)
+		p.Post = bluemonday.UGCPolicy().SanitizeBytes(unsafe)
+		p = append(b, p)
+
+
+	}
+	err = templates.ExecuteTemplate(w, "index.html", &b)
+	checkErr(err)
 
 }
 
 func rssHandler(w http.ResponseWriter, r *http.Request) {
+	db, err := sql.Open("mysql", DATABASE)
+	checkErr(err)
+	defer db.Close()
 
+	query, err := db.Query("select * from post")
+	checkErr(err)
+
+	b := []Page{}
+	for query.Next() {
+		p := Page{}
+		var input string
+		query.Scan(&p.Title, &input, &p.Date, &p.Author, &p.Email)
+		unsafe := blackfriday.MarkdownCommon(input)
+		p.Post = bluemonday.UGCPolicy().SanitizeBytes(unsafe)
+		p = append(b, p)
+
+
+	}
 }
 
 func postHandler(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	post := vars["id"]
+
+	db, err := sql.Open("mysql", DATABASE)
+	checkErr(err)
+	defer db.Close()
+
+	query, err := db.Query("select * from post where name=?", html.EscapeString(post))
+	checkErr(err)
+
+	p := Page{}
+	for query.Next() {
+		var input string
+		query.Scan(&p.Title, &input, &p.Date, &p.Author, &p.Email)
+		unsafe := blackfriday.MarkdownCommon(input)
+		p.Post = bluemonday.UGCPolicy().SanitizeBytes(unsafe)
+
+
+	}
+	err = templates.ExecuteTemplate(w, "post.html", &p)
+	checkErr(err)
 
 }
 
